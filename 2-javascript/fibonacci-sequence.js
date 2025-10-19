eaf31b6a2bce4b7b2e7f731b43649f37d2f8633e
Than Hteik Aung <thanhtaikaung90@gmail.com>
2025-10-20 01:53:16 +0630

    print output fibonacci number according to input variable number

--- 2-javascript/fibonacci-sequence.js
+++ 2-javascript/fibonacci-sequence.js
@@ -1,15 +1,15 @@
 function printFibonacci(n) {
     let fstNumber=0;
     let sndNumber=1;
-    let count=0;
+    let count=1;
 
     do {
         console.log(fstNumber)
         let temp=fstNumber + sndNumber; //1
         fstNumber = sndNumber //1
         sndNumber = temp //1
-    } while (count++ < 10);
+    } while (count++ < n);
 }
 
-printFibonacci(10)
+printFibonacci(5)
 
